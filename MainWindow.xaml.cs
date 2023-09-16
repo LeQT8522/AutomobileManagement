ba8125df9dfd8ac825265089eac71e5bb04cd328
Ha Phi Hung <haphihung55@gmail.com>
2023-09-17 00:42:08 +0700

    Setup basic UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutomobileManagement.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AutomobileManagement
 {
@@ -22,7 +11,13 @@ namespace AutomobileManagement
     {
         public MainWindow()
         {
+            DataContext = App.Current.Services.GetService<MainVM>();
             InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await ((MainVM)DataContext).Load();
         }
     }
 }
